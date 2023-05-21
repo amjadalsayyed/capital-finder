2c45cf4eb5551178ad657c37d7c593cc662e9e9e
amjad alsayyed <amgad.alsayyed@yahoo.com>
2023-05-21 17:42:51 +0300

    feat: requirment

--- api/country.py
+++ api/country.py
@@ -10,15 +10,15 @@ class handler(BaseHTTPRequestHandler):
         path = self.path
         url_comp = parse.urlsplit(path)
         dict_of_res = dict(parse.parse_qsl(url_comp.query))
-        country_name = dict_of_res.get("name")
+        country_name_s = dict_of_res.get("name")
 
 
-        if country_name:
-            url = f"https://restcountries.com/v3.1/name/{country_name}?fullText=true"
+        if country_name_s:
+            url = f"https://restcountries.com/v3.1/name/{country_name_s}?fullText=true"
             res = requests.get(url)
             data = res.json()
             name_of_capital = data[0]["capital"][0]
-            result = f'The capital of {country_name} is {name_of_capital}.'  
+            result = f'The capital of {country_name_s} is {name_of_capital}.'  
 
 
 
